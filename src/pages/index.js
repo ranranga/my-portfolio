5dc9da9fcc4c7fd1b1a5c73edb77ff29cb22c6d7
scadyy <sachidanand.vaishnav@gmail.com>
2019-01-10 11:59:21 +0530

    Code formatting improved and external link correction.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
@@ -9,14 +8,39 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h3>Hello World!</h3>
-    <p>I'm Sachidanand Vaishnav, a passionate <strong>Frontend Engineer</strong> with keen interest in UX and UI design.</p>
-    <p>My work experience can be found here in <Link to="https://www.linkedin.com/in/scadyy/" style={{textDecoration: `none`}}>LinkedIn</Link>. 
-    I'm an active contributor to open source community so    here's my <Link to="https://github.com/scadyy" style={{textDecoration: `none`}}>GitHub
-    </Link> account.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <p>
+      I'm Sachidanand Vaishnav, a passionate <strong>Frontend Engineer</strong>{' '}
+      with keen interest in <strong>UX and UI design</strong>.
+    </p>
+    <p>
+      My work experience can be found here in{' '}
+      <a
+        href="https://www.linkedin.com/in/scadyy/"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ textDecoration: `none` }}
+      >
+        LinkedIn
+      </a>
+      . I'm an active contributor to open source community so here's my{' '}
+      <a
+        href="https://github.com/scadyy"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ textDecoration: `none` }}
+      >
+        GitHub
+      </a>{' '}
+      account.
+    </p>
+    <div style={{ float: `left`, width: `100%` }}>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <div>
+        <p>I love to kill UI and UX problems in a fun way 😎 </p>
+      </div>
     </div>
-    
   </Layout>
 )
 
